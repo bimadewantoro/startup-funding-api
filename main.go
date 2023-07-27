c5afdd6bff63532ff9852615cd71a35014e7f007
Bima Dewantoro <bima.dewantoro@talenavi.com>
2023-07-27 16:41:18 +0700

    chore : [BD] init project. Register user endpoint

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"startup-funding/handler"
+	"startup-funding/user"
+)
+
+func main() {
+	dsn := "root:1234@tcp(127.0.0.1:3306)/startup_funding?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+
+	userHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+	api := router.Group("/api/v1/")
+
+	api.POST("/users", userHandler.RegisterUser)
+
+	router.Run()
+}
